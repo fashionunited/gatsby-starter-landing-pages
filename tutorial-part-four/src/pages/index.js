8d47a9a32bb006b004b779504f09671e8bd09b34
Jelle Rienstra <jelle@fashionunited.com>
2018-01-29 16:13:12 +0100

    Complete part 4

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import g from "glamorous";
+import Link from "gatsby-link"
 
 import { rhythm } from "../utils/typography";
 
@@ -13,11 +14,17 @@ export default ({ data }) => {
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
+        <Link
+        to={node.fields.slug}
+        css={{ textDecoration: `none`, color: `inherit` }}
+      >
+
           <g.H3 marginBottom={rhythm(1 / 4)}>
             {node.frontmatter.title}{" "}
             <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
           </g.H3>
           <p>{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -26,7 +33,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query IndexQuery {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
       totalCount
       edges {
         node {
@@ -34,6 +41,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
