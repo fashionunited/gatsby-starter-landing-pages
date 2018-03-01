d57bff1b6657840f8aa531a54b2b34a7247a0d5a
Joost van der Laan <joost@fashionunited.com>
2018-03-01 14:13:44 +0100

    Fully fixed Emotion with SSR

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,13 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-emotion",
-    // {
-    //   resolve: `gatsby-plugin-google-tagmanager`,
-    //   options: {
-    //     id: `${config.googleTagManagerID}`,
-    //     includeInDevelopment: false
-    //   }
-    // },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: `${config.googleTagManagerID}`,
+        includeInDevelopment: false
+      }
+    },
     "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
@@ -54,9 +54,9 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-transformer-json",
     "gatsby-transformer-yaml",
-    // "gatsby-plugin-catch-links"
-    // "gatsby-plugin-twitter",
-    // "gatsby-plugin-sitemap"
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-twitter",
+    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-transformer-remark",
       options: {
@@ -75,108 +75,108 @@ module.exports = {
           "gatsby-remark-autolink-headers"
         ]
       }
-    }
-    // {
-    //   resolve: "gatsby-plugin-nprogress",
-    //   options: {
-    //     color: config.themeColor
-    //   }
-    // }
+    },
+    {
+      resolve: "gatsby-plugin-nprogress",
+      options: {
+        color: config.themeColor
+      }
+    },
 
-    // {
-    //   resolve: "gatsby-plugin-manifest",
-    //   options: {
-    //     name: config.siteTitle,
-    //     short_name: config.siteTitle,
-    //     description: config.siteDescription,
-    //     start_url: config.pathPrefix,
-    //     background_color: config.backgroundColor,
-    //     theme_color: config.themeColor,
-    //     display: "minimal-ui",
-    //     icons: [
-    //       {
-    //         src: "/logos/logo-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png"
-    //       },
-    //       {
-    //         src: "/logos/logo-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png"
-    //       }
-    //     ]
-    //   }
-    // },
-    // "gatsby-plugin-offline",
-    // {
-    //   resolve: "gatsby-plugin-feed",
-    //   options: {
-    //     setup(ref) {
-    //       const ret = ref.query.site.siteMetadata.rssMetadata;
-    //       ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-    //       ret.generator = "GatsbyJS Material Starter";
-    //       return ret;
-    //     },
-    //     query: `
-    //     {
-    //       site {
-    //         siteMetadata {
-    //           rssMetadata {
-    //             site_url
-    //             feed_url
-    //             title
-    //             description
-    //             image_url
-    //             author
-    //             copyright
-    //           }
-    //         }
-    //       }
-    //     }
-    //   `,
-    //     feeds: [
-    //       {
-    //         serialize(ctx) {
-    //           const rssMetadata = ctx.query.site.siteMetadata.rssMetadata;
-    //           return ctx.query.allMarkdownRemark.edges.map(edge => ({
-    //             categories: edge.node.frontmatter.tags,
-    //             date: edge.node.frontmatter.date,
-    //             title: edge.node.frontmatter.title,
-    //             description: edge.node.excerpt,
-    //             author: rssMetadata.author,
-    //             url: rssMetadata.site_url + edge.node.fields.slug,
-    //             guid: rssMetadata.site_url + edge.node.fields.slug,
-    //             custom_elements: [{ "content:encoded": edge.node.html }]
-    //           }));
-    //         },
-    //         query: `
-    //         {
-    //           allMarkdownRemark(
-    //             limit: 1000,
-    //             sort: { order: DESC, fields: [frontmatter___date] },
-    //           ) {
-    //             edges {
-    //               node {
-    //                 excerpt
-    //                 html
-    //                 timeToRead
-    //                 fields { slug }
-    //                 frontmatter {
-    //                   title
-    //                   cover
-    //                   date
-    //                   category
-    //                   tags
-    //                 }
-    //               }
-    //             }
-    //           }
-    //         }
-    //       `,
-    //         output: config.siteRss
-    //       }
-    //     ]
-    //   }
-    // }
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitle,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: "minimal-ui",
+        icons: [
+          {
+            src: "/logos/logo-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logos/logo-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    },
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        setup(ref) {
+          const ret = ref.query.site.siteMetadata.rssMetadata;
+          ret.allMarkdownRemark = ref.query.allMarkdownRemark;
+          ret.generator = "GatsbyJS Material Starter";
+          return ret;
+        },
+        query: `
+        {
+          site {
+            siteMetadata {
+              rssMetadata {
+                site_url
+                feed_url
+                title
+                description
+                image_url
+                author
+                copyright
+              }
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize(ctx) {
+              const rssMetadata = ctx.query.site.siteMetadata.rssMetadata;
+              return ctx.query.allMarkdownRemark.edges.map(edge => ({
+                categories: edge.node.frontmatter.tags,
+                date: edge.node.frontmatter.date,
+                title: edge.node.frontmatter.title,
+                description: edge.node.excerpt,
+                author: rssMetadata.author,
+                url: rssMetadata.site_url + edge.node.fields.slug,
+                guid: rssMetadata.site_url + edge.node.fields.slug,
+                custom_elements: [{ "content:encoded": edge.node.html }]
+              }));
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    timeToRead
+                    fields { slug }
+                    frontmatter {
+                      title
+                      cover
+                      date
+                      category
+                      tags
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: config.siteRss
+          }
+        ]
+      }
+    }
   ]
 };
