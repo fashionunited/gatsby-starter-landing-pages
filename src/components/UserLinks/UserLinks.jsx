a0c5598bd2f791215b36cc380aacfbf6f6a1534e
Joost van der Laan <joostvanderlaan@gmail.com>
2018-03-16 14:05:04 +0100

    userlinks style

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styled, { css } from "react-emotion";
 
+import { Button } from "rmwc/Button";
+
 const StyledDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -14,9 +16,9 @@ class UserLinks extends Component {
     const { userLinks } = this.props.config;
     const { labeled } = this.props;
     return userLinks.map(link => (
-      <button key={link.label} href={link.url}>
+      <Button key={link.label} href={link.url}>
         {labeled ? link.label : ""}
-      </button>
+      </Button>
     ));
   }
   render() {
